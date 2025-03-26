54722d48b5811a3ee5fd58edcd244579fb7ebbc5
uschrono <junin_gym@hotmail.com>
2025-03-26 13:10:56 -0300

    adiciconando select.

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -98,4 +98,21 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.get('.success').should('be.visible')
   })
+
+  it('seleciona um produto (Youtube) por seu texto', () => {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor', () => {
+    cy.get('#product')
+      .select('Mentoria')
+      .should('have.value', 'mentoria')
+  })
+  it.only('seleciona um produto (Blog) por seu valor', () => {
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog')
+  })
 })
